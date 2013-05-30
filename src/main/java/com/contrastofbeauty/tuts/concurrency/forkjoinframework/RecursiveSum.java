a1f1a004144e63a382a6bec65444c38860c88b8a
blackat <eugenio.lentini@gmail.com>
2013-05-30 20:45:18 +0200

    fork join framework

---
+++ src/main/java/com/contrastofbeauty/tuts/concurrency/forkjoinframework/RecursiveSum.java
@@ -0,0 +1,16 @@
+package com.contrastofbeauty.tuts.concurrency.forkjoinframework;
+
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: eugenio
+ * Date: 28/04/13
+ */
+public class RecursiveSum extends RecursiveTask<Long> {
+    @Override
+    protected final Long compute() {
+        return null;  //To change body of implemented methods use File |
+        // Settings | File Templates.
+    }
+}
